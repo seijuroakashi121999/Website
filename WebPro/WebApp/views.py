09089f454b279a0021abfe8c235da4fe63b49ceb
seijuroakashi121999 <seijuroakashi121999@gmail.com>
2018-07-10 22:32:53 +0530

    Ready

--- WebPro/WebApp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    return render(request,'index.html')
-
-def Rankings(request):
-    return render(request,"Rankings.html")
